b109c7def8f4b578fecaa982c67ce20e82b62975
philip greenwald <pghrpg@gmail.com>
2025-02-15 16:46:11 -0600

    added create action to tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,17 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+
+	def create
+		print "fire fire fire fire"
+		print params["tweet"]["content"]
+		print "cold cold cold cold"
+		@tweet = Tweet.new
+		@tweet.content = params["tweet"]["content"]
+		@tweet.save
+
+		redirect_to new_tweet_path
+		# this is  the code that will create a new tweet
+	end
 end
