fd1d8004031df5269f5f9b32632e3210a9c0db7f
philip greenwald <pghrpg@gmail.com>
2025-02-15 12:26:16 -0600

    generated static_pages controller and associated views

---
+++ app/controllers/flags_controller.rb
@@ -0,0 +1,11 @@
+class FlagsController < ApplicationController
+
+	def show
+		@tweet = "apple pie is the best pie. scratch that, i cannot tell a lie."
+		@jerry_reed_is_still_popular = false
+		@number1 = "1"
+		@number2 = "2"
+
+	end
+
+end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+	def index
+		@dog = "p terry"
+	end
+end
